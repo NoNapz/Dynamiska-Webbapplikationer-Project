b0cdc215eccbb47832f854f1f2fa24627c174fc5
Robin Hellström <robin1robin@hotmail.com>
2021-01-02 20:29:57 +0100

    There we go, fixed.

--- database.js
+++ database.js
@@ -243,7 +243,7 @@ const getReplyLikes = async (replyID) => {
             [replyID]
         );
         const likeAmount = getLikes.length;
-        console.log("Current likes: " + likeAmount + ", REPLY ID:" + postID);
+        console.log("Current likes: " + likeAmount + ", REPLY ID:" + replyID);
         const updateLikes = await dbCon.get(
             `UPDATE reply SET likes = ? WHERE replyID = ?`,
             [likeAmount, replyID]
